b080d3ea1a33641b01ceed43ae2b3f83f400a880
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-04-30 13:47:16 +0300

    Update api_token_scanner.py

--- api_token_scanner.py
+++ api_token_scanner.py
@@ -18,7 +18,7 @@ from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
-HTTP_REFERER = ""https://www.google.com""
+HTTP_REFERER = "https://www.google.com"
 
 
 # access_token,refresh_token
