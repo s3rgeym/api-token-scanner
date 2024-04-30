a0a9107933bca275e3538fc0b46f9475f2f527aa
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2024-04-30 05:20:27 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Api Token Scanner
+
+```bash
+git clone https://github.com/s3rgeym/api-token-scanner.git
+cd api-token-scanner
+pipx install --force .
+api-token-scanner -h
+```
---
+++ api_token_scanner.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+import argparse
+import asyncio
+import dataclasses
+import json
+import logging
+import re
+import sys
+import urllib.parse as uparse
+from collections import deque
+from contextlib import asynccontextmanager, suppress
+from email.message import Message
+from typing import AsyncIterator, Iterator, Sequence, TextIO
+
+import aiohttp
+from bs4 import BeautifulSoup
+
+logger = logging.getLogger(__name__)
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+
+
+# access_token,refresh_token
+TOKEN_NAME = (
+    r"\S*("
+    + "|".join(
+        re.escape(x).replace("_", "_?") for x in ["token", "api_key", "client_secret"]
+    )
+    + ")"
+)
+
+TOKEN_VALUE = r"\w{30,}"
+
+TOKEN_RE = re.compile(
+    "|".join(
+        [
+            f"(?P<url_param>{TOKEN_NAME}={TOKEN_VALUE})",
+            rf"(?P<variable_assign>{TOKEN_NAME}\s*=\s*[\"']{TOKEN_VALUE})",
+            rf"(?P<object_property>{TOKEN_NAME}['\"]?\s*:\s*['\"]{TOKEN_VALUE})",
+        ]
+    ),
+    re.IGNORECASE,
+)
+
+
+@dataclasses.dataclass
+class ApiTokenScanner:
+    _: dataclasses.KW_ONLY
+    depth: int = 1
+    output: TextIO = sys.stdout
+    parallel: int = 10
+    timeout: float = 15.0
+
+    def create_session(self) -> aiohttp.ClientSession:
+        con = aiohttp.TCPConnector(ssl=False)
+        tmt = aiohttp.ClientTimeout(self.timeout)
+        session = aiohttp.ClientSession(connector=con, timeout=tmt)
+        session.headers.update({"User-Agent": USER_AGENT})
+        return session
+
+    async def get_session(self) -> aiohttp.ClientSession:
+        while True:
+            try:
+                return self.sessions.popleft()
+            except IndexError:
+                await asyncio.sleep(0.1)
+
+    @asynccontextmanager
+    async def acquire_session(self) -> AsyncIterator[aiohttp.ClientSession]:
+        session = await self.get_session()
+        try:
+            yield session
+        finally:
+            self.release_session(session)
+
+    def release_session(self, session: aiohttp.ClientSession) -> None:
+        self.sessions.appendleft(session)
+
+    async def run(self, urls: list[str]) -> None:
+        q = asyncio.Queue()
+
+        for x in urls:
+            q.put_nowait((x, self.depth))
+
+        self.create_sessions()
+        seen = set()
+
+        tasks = [
+            asyncio.create_task(self.process(q, seen)) for _ in range(self.parallel)
+        ]
+
+        await q.join()
+
+        for _ in range(self.parallel):
+            q.put_nowait(None)
+
+        await asyncio.wait(tasks)
+
+        logger.info("all tasks finished!")
+
+    async def process(
+        self, q: asyncio.Queue[tuple[str, int] | None], seen: set[str]
+    ) -> None:
+        while True:
+            async with self.acquire_session() as session:
+                try:
+                    item = await q.get()
+
+                    if item is None:
+                        # ERROR:asyncio:Unclosed client session
+                        await session.close()
+                        break
+
+                    url, depth = item
+
+                    if url in seen:
+                        logger.debug("already seen: %s", url)
+                        continue
+
+                    await self.fetch(session, url, depth, q, seen)
+                finally:
+                    q.task_done()
+
+    async def fetch(
+        self,
+        session: aiohttp.ClientSession,
+        url: str,
+        depth: int,
+        q: asyncio.Queue[tuple[str, int] | None],
+        seen: set[str],
+    ) -> None:
+        logger.debug("fetch url: %s", url)
+
+        try:
+            response = await session.get(url)
+            response.raise_for_status()
+        except (aiohttp.ClientError, asyncio.TimeoutError) as ex:
+            logger.error(ex)
+            return
+        finally:
+            seen.add(url)
+
+        if ct := response.headers.get("Content-Type"):
+            ct, _ = parse_content_type(ct)
+
+        # As of May 2022, text/javascript is the preferred type once again (see RFC 9239)
+        # https://stackoverflow.com/a/73542396/2240578
+        # if ct not in [
+        #     "text/html",
+        #     "application/javascript",
+        #     "text/javascript",
+        # ]:
+        #     logger.debug("unexpected content-type: %s", ct)
+        #     return
+
+        content = await response.text()
+
+        cur_url = str(response.url)
+
+        if ct == "text/html" and depth > 0:
+            links = self.extract_links(content)
+            await self.process_links(links, cur_url, depth - 1, q, seen)
+
+        for k, v in self.find_tokens(content):
+            json.dump(
+                {"kind": k, "match": v, "url": cur_url},
+                self.output,
+                ensure_ascii=False,
+            )
+            self.output.write("\n")
+            self.output.flush()
+
+    def find_tokens(self, content: str) -> Iterator[tuple[str, str]]:
+        for m in TOKEN_RE.finditer(content):
+            for k, v in m.groupdict().items():
+                if v is not None:
+                    yield (k, v)
+                    break
+
+    def extract_links(self, content: str) -> set[str]:
+        soup = BeautifulSoup(content, features="lxml")
+        links = set()
+
+        for el in soup.find_all("a", href=True, download=False):
+            links.add(el["href"])
+
+        for el in soup.find_all("script", src=True):
+            links.add(el["src"])
+
+        return links
+
+    async def process_links(
+        self,
+        links: set[str],
+        base_url: str,
+        depth: int,
+        q: asyncio.Queue[tuple[str, int] | None],
+        seen: set[str],
+    ) -> None:
+        sp = uparse.urlsplit(base_url)
+
+        for link in links:
+            url = uparse.urljoin(base_url, link)
+
+            if sp.netloc != uparse.urlsplit(url).netloc:
+                continue
+
+            if url in seen:
+                continue
+
+            await q.put((url, depth))
+
+    def create_sessions(self) -> None:
+        logger.debug("create sessions")
+
+        self.sessions = deque(
+            iterable=(self.create_session() for x in range(self.parallel)),
+            maxlen=self.parallel,
+        )
+
+
+def parse_content_type(content_type: str) -> tuple[str, dict[str, str]]:
+    email = Message()
+    email["Content-Type"] = content_type
+    params = email.get_params()
+    return params[0][0], dict(params[1:])
+
+
+def main(argv: Sequence[str] = None) -> None:
+    parser, args = _parse_args(argv)
+
+    urls = list(filter(None, map(str.strip, args.input)))
+
+    if not urls:
+        parser.error("nothing to scan")
+
+    logging.basicConfig()
+
+    lvl = max(logging.DEBUG, logging.WARNING - logging.DEBUG * args.verbose)
+    logger.setLevel(level=lvl)
+
+    scanner = ApiTokenScanner(
+        depth=args.depth,
+        output=args.output,
+        parallel=args.parallel,
+        timeout=args.timeout,
+    )
+
+    with suppress(KeyboardInterrupt):
+        asyncio.run(scanner.run(urls))
+
+
+class NameSpace(argparse.Namespace):
+    input: TextIO
+    output: TextIO
+    depth: int
+    parallel: int
+    timeout: float
+    verbose: int
+
+
+def _parse_args(argv: Sequence[str]) -> tuple[argparse.ArgumentParser, NameSpace]:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        "--in",
+        help="input",
+        type=argparse.FileType(),
+        default="-",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        "--out",
+        help="output",
+        type=argparse.FileType("w"),
+        default="-",
+    )
+    parser.add_argument(
+        "-d",
+        "--depth",
+        help="max crawling depth",
+        type=int,
+        default=1,
+    )
+    parser.add_argument(
+        "-p",
+        "--parallel",
+        help="number of parallel tasks",
+        type=int,
+        default=10,
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        help="client timeout",
+        type=float,
+        default=15.0,
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="be more verbose (-vv for debug)",
+        action="count",
+        default=0,
+    )
+    return parser, parser.parse_args(argv, namespace=NameSpace())
+
+
+if __name__ == "__main__":
+    main()
